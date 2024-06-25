66565ceb62c33ca65920eb5a605c325d7714a9ee
bca-master <vagrant@hellocloud>
2024-06-25 09:41:50 +0000

    Add Terraform files

---
+++ version.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.55.0"
+    }
+  }
+}
+
+provider "aws" {
+  shared_config_files      = ["/home/vagrant/.aws/config"]
+  shared_credentials_files = ["/home/vagrant/.aws/credentials"]
+  profile                  = "bca-master"
+}
